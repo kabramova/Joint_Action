fd4faf75ec69a2b495d022126a6ce4ec05ef615a
SHEscher <zimon@gmx.com>
2016-04-22 17:20:01 +0200

    Test3 from server

--- Server_Sim.py
+++ Server_Sim.py
@@ -19,4 +19,4 @@ Fitness_progress = e2.run_evolution(Generations=500, mutation_var=.02, complex_t
 # e2.reimplement_population(Filename=None, Plot=True) # Filename starts with "sim..."
 
 
-# Test from server
+# Test 3
