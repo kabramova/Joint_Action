08c87c74234279e8ca9473eaf5a6aa3f181efecb
SHEscher <zimon@gmx.com>
2016-05-25 20:32:44 +0200

    joint_action

--- JA_Server_Sim.py
+++ JA_Server_Sim.py
@@ -3,7 +3,7 @@ from JA_Evolution import *
 audicon = audio_condition_request()
 number_of_generations = generation_request()
 
-ja = JA_Evolution(auditory_condition=audicon, pop_size=10)
+ja = JA_Evolution(auditory_condition=audicon, pop_size=55)
 
 filename = filename_request("joint")
 
