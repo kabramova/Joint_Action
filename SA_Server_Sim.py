1164a234c620310f08c08526af3810b86a003612
SHEscher <zimon@gmx.com>
2016-05-25 22:02:50 +0200

    joint_action

--- SA_Server_Sim.py
+++ SA_Server_Sim.py
@@ -21,11 +21,11 @@ print("Run Evolution for {} Generations in Sound Condition={}".format(number_of_
 sa.run_evolution(generations=number_of_generations)
 
 
-# Reimplement and Plot
-sa2 = SA_Evolution(auditory_condition=False)
-print("Sound_Cond:", sa2.condition,", Popsize:", sa2.pop_size,", Gen:", sa2.generation)
-filename = filename_request("single")
-output = sa2.reimplement_population(filename=filename, Plot=True)
-print("Sound_Cond:", sa2.condition,", Popsize:", sa2.pop_size,", Gen:", sa2.generation)
-sa2.print_best(5)
-# print(np.round(sa2.pop_list[0:50,0:2],2))
+# # Reimplement and Plot
+# sa2 = SA_Evolution(auditory_condition=False)
+# print("Sound_Cond:", sa2.condition,", Popsize:", sa2.pop_size,", Gen:", sa2.generation)
+# filename = filename_request("single")
+# output = sa2.reimplement_population(filename=filename, Plot=True)
+# print("Sound_Cond:", sa2.condition,", Popsize:", sa2.pop_size,", Gen:", sa2.generation)
+# sa2.print_best(5)
+# # print(np.round(sa2.pop_list[0:50,0:2],2))
