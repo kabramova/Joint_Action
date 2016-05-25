1a4b3519c755d4f13a7f7f693611e046d58a0ae3
SHEscher <zimon@gmx.com>
2016-05-25 19:26:58 +0200

    joint_action

--- JA_Server_Sim.py
+++ JA_Server_Sim.py
@@ -1,5 +1,3 @@
-# TODO: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
 from JA_Evolution import *
 
 audicon = audio_condition_request()
@@ -23,15 +21,15 @@ print("Run Evolution for {} Generations in Sound Condition={}".format(number_of_
 ja.run_evolution(generations=number_of_generations)
 
 
-# Reimplement and Plot
-ja2 = JA_Evolution(auditory_condition=True)
-print("Sound_Cond:", ja2.condition,", Popsize:", ja2.pop_size,", Gen:", ja2.generation)
-filename = filename_request("joint")
-output = ja2.reimplement_population(filename=filename, Plot=True)
-print("Sound_Cond:", ja2.condition,", Popsize:", ja2.pop_size,", Gen:", ja2.generation)
-ja2.print_best(5)
-# print(np.round(ja2.pop_list_L[0:50,0:2],2))
-# print(np.round(ja2.pop_list_R[0:50,0:2],2))
+# # Reimplement and Plot
+# ja2 = JA_Evolution(auditory_condition=True)
+# print("Sound_Cond:", ja2.condition,", Popsize:", ja2.pop_size,", Gen:", ja2.generation)
+# filename = filename_request("joint")
+# output = ja2.reimplement_population(filename=filename, Plot=True)
+# print("Sound_Cond:", ja2.condition,", Popsize:", ja2.pop_size,", Gen:", ja2.generation)
+# ja2.print_best(5)
+# # print(np.round(ja2.pop_list_L[0:50,0:2],2))
+# # print(np.round(ja2.pop_list_R[0:50,0:2],2))
 
 
 
