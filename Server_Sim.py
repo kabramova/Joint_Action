c073e83e810c19940cc546d2bfda07bc87aa8415
SHEscher <zimon@gmx.com>
2016-05-26 12:19:29 +0200

    evol

--- Server_Sim.py
+++ Server_Sim.py
@@ -1,23 +1,11 @@
 from Evolution import *
 
-
 ### Simulation of Evolution
-# e1 = Evolution(simlength=5000)
-# Fitness_progress = e1.run_evolution(Generations=500, mutation_var=.0001, complex_trials=True, fit_prop_sel=True, position_agent=[50,50], angle_to_target= np.pi/2,  distance_to_target = 30)
-
-# e1.reimplement_population(Filename=None, Plot=True) # Filename starts with "sim..."
-
-'''
-XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
-XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
-XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
-'''
 
 e2 = Evolution(simlength=5000)
-e2.filename = "sim5000.mut0.02.Gen10001-11000_CT=True.fps=False"
+e2.filename = "sim5000.mut0.02.Gen11001-12000_CT=True.fps=False"
 e2.reimplement_population(Filename=None, Plot=False)
 e2.run_evolution(Generations=1000, mutation_var=.02, complex_trials=True, fit_prop_sel=False, position_agent=[50,50], angle_to_target= np.pi/2,  distance_to_target = 30)
-
 
 # import os
 # os.listdir()
