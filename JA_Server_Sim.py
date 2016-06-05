10c827f1a914b05f883dbc2cd7274570b08a460d
SHEscher <zimon@gmx.com>
2016-06-05 17:39:21 +0200

    Joint

--- JA_Server_Sim.py
+++ JA_Server_Sim.py
@@ -22,7 +22,7 @@ else: # if splitter is used, these values must be pre-given, here in python file
     # Manually adjust the following parameters:
     audicon = False
     number_of_generations = 1000
-    filename = "Gen501-1500.popsize55.mut0.02.sound_cond=False.JA.joint(Fitness7.43)" # or None
+    filename = "Gen1501-2500.popsize55.mut0.02.sound_cond=False.JA.joint(Fitness7.34)" # or None
     print("Splitter {} started!".format(split))
 
 
