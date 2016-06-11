64b958384ba798c667e2ab19980171efaf9f7ed6
SHEscher <zimon@gmx.com>
2016-06-11 21:39:57 +0200

    Single

--- SA_Server_Sim.py
+++ SA_Server_Sim.py
@@ -19,9 +19,9 @@ if not split: # is False
 
 else: # if splitter is used, these values must be pre-given, here in python file
     # Manually adjust the following parameters:
-    audicon = True
-    number_of_generations = 500
-    filename = "Gen2501-3250.popsize111.mut0.02.sound_cond=True.JA.single(Fitness7.29)" # or None
+    audicon = False
+    number_of_generations = 250
+    filename = "Gen501-1000.popsize111.mut0.02.sound_cond=False.JA.single(Fitness7.69)" # or None
     print("Splitter {} started!".format(split))
 
 
