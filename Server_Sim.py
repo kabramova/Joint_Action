3821a6221589fa9eca573d7c03ac126cb211f17e
SHEscher <zimon@gmx.com>
2016-08-04 17:13:03 +0200

    Joint Action

--- Server_Sim.py
+++ Server_Sim.py
@@ -15,7 +15,7 @@ __status__ = "Development"
 e1 = Evolution(simlength=5000)
 
 # os.listdir('poplists')
-filename = "sim5000.mut0.02.Gen10001-12000(Fitness 24.45)"      # Filename starts with "sim..."
+filename = None  # "sim5000.mut0.02.Gen12001-15000(Fitness 24.45)"      # Filename starts with "sim..."
 if filename != "" and isinstance(filename, str):
     e1.reimplement_population(filename=filename, plot=False)
     print("File implemented:", e1.filename)
@@ -23,8 +23,8 @@ if filename != "" and isinstance(filename, str):
 n_gen = generation_request()
 
 print("Run Evolution from Generations {}-{}".format(e1.generation, e1.generation+n_gen))
-e1.run_evolution(generations=n_gen, mutation_var=.02, complex_trials=True, fit_prop_sel=False, position_agent=[50, 50],
-                 angle_to_target=np.pi/2, distance_to_target=30)
+e1.run_evolution(generations=n_gen, mutation_var=.25, complex_trials=True, fit_prop_sel=False, position_agent=[50, 50],
+                 angle_to_target=np.pi/2, distance_to_target=30)  # mutation rate default 0.01
 
 
 # Plot current file:
